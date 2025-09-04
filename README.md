bcb1c026556f5fb41c2ef0a50ff495e17606d2d1
xaul31 <nelson.villagran@inacapmail.cl>
2025-09-04 14:10:39 -0400

    se agrego la funcion listar_contactos

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 
 # agenda de contacto
 proyecto simple de consola para registrar, buscar y listar contactos usando python
+
+## Agenda.py
+    se creo la funcion de agregar contacto
+    se creo la funcion para listar contactos
--- agenda.py
+++ agenda.py
@@ -5,6 +5,15 @@ contacto =[]
 def agregar_contactos(nombre,telefono):
     contacto.append({"nombre": nombre,"telefono:":telefono})
 
+
+def listar_contactos():
+    print("\nlista de contactos : ")
+    for c in contacto:
+        print(f"{c['nombre']} - {c['telefono']}")
+
+listar_contactos()
+
+
 nombre = input("ingresar el nombre : ")
 telefono = input("ingresar el telefono : ")
 agregar_contactos(nombre,telefono)
